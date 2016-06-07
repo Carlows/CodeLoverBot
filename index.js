ab48d866f61b3f63399ca191dec52ad0291ade67
Carlos <carlos@hashlabs.com>
2016-06-07 15:00:34 -0400

    Initial bot configuration

---
+++ index.js
@@ -0,0 +1,24 @@
+var TelegramBot = require('node-telegram-bot-api');
+var Cleverbot = require('cleverbot-node');
+
+var token = process.env.BOT_TOKEN;
+
+var bot = new TelegramBot(token, { polling: true });
+var cleverbot = new Cleverbot;
+
+bot.onText(/\/hi/, function(msg, match) {
+  var chatId = msg.chat.id;
+  console.log('called ' + chatId);
+  bot.sendMessage(chatId, "Luque es marico");
+});
+
+bot.onText(/\/question (.+)/, function(msg, match) {
+  var chatId = msg.chat.id;
+  var receivedText = match[1];
+
+  Cleverbot.prepare(function(){
+    cleverbot.write(receivedText, function (response) {
+         bot.sendMessage(chatId, response.message);
+    });
+  });
+});
