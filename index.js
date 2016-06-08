1ab910650d02ac30d3e31359e0bf5a89bdd25906
Carlos Martinez <carlosjesus94@gmail.com>
2016-06-07 21:46:40 -0400

    Add faggot command

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var TelegramBot = require('node-telegram-bot-api');
 var Cleverbot = require('cleverbot-node');
+var format = require('string-template');
 
 var token = process.env.BOT_TOKEN;
 
 var bot = new TelegramBot(token, { polling: true });
 var cleverbot = new Cleverbot;
 
-bot.onText(/\/hi/, function(msg, match) {
+var arrayOfMembers = process.env.MEMBERS.split(',');
+
+bot.onText(/\/faggot/, function(msg, match) {
   var chatId = msg.chat.id;
-  console.log('called ' + chatId);
-  bot.sendMessage(chatId, "Luque es marico");
+  var faggot = arrayOfMembers[Math.floor(Math.random() * arrayOfMembers.length)];
+  bot.sendMessage(chatId, format("{name} es marico", { name: faggot }));
 });
 
-bot.onText(/\/question (.+)/, function(msg, match) {
+bot.onText(/\/hey (.+)/, function(msg, match) {
   var chatId = msg.chat.id;
   var receivedText = match[1];
 
@@ -21,12 +24,4 @@ bot.onText(/\/question (.+)/, function(msg, match) {
          bot.sendMessage(chatId, response.message);
     });
   });
-});
-
-bot.on('message', function(msg) {
-  var chatId = msg.chat.id;
-
-  if(msg.sticker) {
-    bot.sendMessage(chatId, "msg was a sticker");
-  }
 });
